bfeff58dd77bb0814e861343a99cf29ac8668ebe
林靖詞 <100269218+oFHRo@users.noreply.github.com>
2022-06-21 02:27:47 +0800

    Delete HW7 directory

--- HW7/src/Components/UserName.js
+++
@@ -1,8 +0,0 @@
-const username = () => {
-    var output = <div><img src={require('./UserName_Image.png')} class="UserName_Image" />
-        <input class="username" onChange={(usertext) => { console.log(usertext.target.value) }}></input></div>
-
-    return output;
-}
-
-export default username;
