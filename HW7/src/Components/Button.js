b7fc3ae5b432ea0437a3ebf51e64b4ec9fda11e0
林靖詞 <100269218+oFHRo@users.noreply.github.com>
2022-06-21 15:44:23 +0800

    Delete HW7 directory

--- HW7/src/Components/Button.js
+++
@@ -1,8 +0,0 @@
-const button = () => {
-    var output = <div><button type="button" class="button_border" onClick={() => { alert('Login Successful !') }}>
-        <img src={require('./Button_Image.png')} class="Button_Image" /></button></div>
-
-    return output;
-}
-
-export default button;
--- HW7/src/Components/Login.js
+++
@@ -1,7 +0,0 @@
-const login = () => {
-    var output = <div><img src={require('./Login_Image.png')} class="Login_Image" /></div>
-
-    return output;
-}
-
-export default login;
